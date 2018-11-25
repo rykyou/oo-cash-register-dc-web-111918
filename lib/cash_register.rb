432046ee3a83592d5e8d2850ba34ed9e10ddd0e8
Robin Kim <rkim1214@gmail.com>
2018-11-25 04:20:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,25 +27,4 @@ class CashRegister
     @total = 0
   end 
   
-end 
-
-  # attr_accessor :total, :discount
-  
-  # def initialize(total=0, discount=0)
-  #   @total = total 
-  #   if discount
-  #     @discount = 20
-  #   end 
-  # end 
-  
-  # def add_item(item, price, quantity=1)
-  #   @total = @total + price * quantity
-  # end 
-  
-  # def apply_discount
-  #   @total = @total - @total * @discount/100
-  # end
-  
-  # def total
-  #   @total
-  # end
+end
