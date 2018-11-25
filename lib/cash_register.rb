a2c88a1dac4b6dc9bd183270dad256aa09ed1ca0
Robin Kim <rkim1214@gmail.com>
2018-11-25 04:09:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
   
   def initialize(discount=0)
     @total = 0
-    @discount = 20 if discount
+    @discount = 0 if discount
   end 
   
   def add_item(item, price, quantity=1) 
