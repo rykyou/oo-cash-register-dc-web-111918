1863cf75a1e9dbd29fa598d1cf91e657000639e7
Robin Kim <rkim1214@gmail.com>
2018-11-25 03:54:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,9 @@ require 'pry'
 class CashRegister
   def initialize(total=0, discount=0)
     @total = total
-    @discount = 20 if discount
+    if discount
+      @discount = 20
+    end 
   end 
 end 
 
